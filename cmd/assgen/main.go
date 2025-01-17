44013ee6b7ba150a1ef9887e876416ceff1c0ca7
Levon Kirakosyan <noosx.e@gmail.com>
2025-01-16 21:16:53 -0800

    implement asset copying and hashing

--- cmd/assgen/main.go
+++ cmd/assgen/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -30,7 +35,7 @@ func run(stdout, stderr io.Writer, args []string) (code int) {
 	}
 	switch args[1] {
 	case "generate":
-		return generate(stdout, stderr, args[2:])
+		return generate(args[2:])
 	case "help", "-help", "--help", "-h":
 		fmt.Fprint(stdout, usage)
 		return 0
@@ -39,24 +44,133 @@ func run(stdout, stderr io.Writer, args []string) (code int) {
 	return 64 // EX_USAGE
 }
 
+type Config struct {
+	Styles  []string `yaml:"styles"`
+	Scripts []string `yaml:"scripts"`
+	Out     *string  `yaml:"out"`
+}
+
 const usageGenerate = `usage: assgen generate [<args>...]
 
+Generates the manifest file
+
 Args:
-	-path <path>
-		Generates manifest for all assets in path.
+	-config
+		Config file path. (default ./assgen.yaml)
+	-out
+		Specify the output directory. (default ./dist)
 `
 
-func generate(stdout, stderr io.Writer, args []string) (code int) {
+func generate(args []string) (code int) {
 	cmd := flag.NewFlagSet("generate", flag.ExitOnError)
 
-	pathFlag := cmd.String("path", "", "")
+	var config string
+	cmd.StringVar(&config, "config", "./assgen.yaml", "config file path")
 
+	var out string
+	cmd.StringVar(&out, "out", "./dist", "output directory")
+
+	cmd.Usage = func() { fmt.Print(usageGenerate) }
 	err := cmd.Parse(args)
 	if err != nil {
-		fmt.Fprint(stderr, usageGenerate)
+		cmd.PrintDefaults()
+		return 64
 	}
 
-	fmt.Println(*pathFlag)
+	configFilePath, err := filepath.Abs(config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	configFileDir := filepath.Dir(configFilePath)
+	outputPath := filepath.Join(configFileDir, out)
+
+	log.Println("ensuring output path")
+	_, err = os.Stat(outputPath)
+	if os.IsNotExist(err) {
+		log.Println("output path does not exist, creating...")
+		err := os.Mkdir(outputPath, 0755)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("output path created")
+	} else {
+		log.Println("output path exists")
+	}
+
+	configDoc, err := os.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatal(err)
+		return 1
+	}
+
+	c := Config{}
+
+	err = yaml.Unmarshal(configDoc, &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scripts := c.Scripts
+	styles := c.Styles
+
+	log.Println("processing scripts")
+	err = processGlobs(scripts, configFileDir, outputPath)
+
+	log.Println("processing styles")
+	err = processGlobs(styles, configFileDir, outputPath)
 
 	return 0
+}
+
+func processGlobs(globs []string, configFileDir string, outputPath string) error {
+	for _, script := range globs {
+		fullpath := filepath.Join(configFileDir, script)
+		matches, err := filepath.Glob(fullpath)
+		if err != nil {
+			return err
+		}
+
+		for _, match := range matches {
+			rel, err := filepath.Rel(configFileDir, match)
+			if err != nil {
+				return err
+			}
+
+			destPath := filepath.Join(outputPath, rel)
+			log.Printf("copying %s\n", rel)
+			hash, err := copyFile(match, destPath)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(hash)
+		}
+	}
+
+	return nil
+}
+
+func copyFile(from string, to string) (string, error) {
+	src, err := os.Open(from)
+	if err != nil {
+		return "", err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(to)
+	if err != nil {
+		return "", err
+	}
+	defer dst.Close()
+
+	hasher := sha256.New()
+	data := io.TeeReader(src, hasher)
+
+	_, err = io.Copy(dst, data)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hasher.Sum(nil)), nil
 }
